0a8548189f92838e21c50d044269e1a7f47cdbad
QoQoQ1 <96755504+QoQoQ1@users.noreply.github.com>
2021-12-28 04:04:08 +0200

    Update TG_User_Finder.py

--- TG_User_Finder.py
+++ TG_User_Finder.py
@@ -38,7 +38,7 @@ def list_checker():
 
 if __name__ == '__main__':
     phone = '+201062891225'
-    client = TelegramClient(phone,18733945,'85f6ec0afc486c5c46f3f485ac1042fd')
+    client = TelegramClient(phone, 18733945, '85f6ec0afc486c5c46f3f485ac1042fd')
     client.connect()
     if not client.is_user_authorized():
         client.send_code_request(phone)
